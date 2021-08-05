9f574f9211003c3564d5d3f5acda8b7a59825cf5
pranav21999 <pranavdani99@gmail.com>
2021-08-05 23:39:43 +0530

    Added:programs of  Functios

---
+++ Function/Palindrome.js
@@ -0,0 +1,27 @@
+const readline = require("prompt-sync");
+const prompt=readline();
+
+function palindrome(num1){
+     while( num1 > 0) {
+            lastDgit = (num1 % 10);
+            console.log("la"+lastDgit);
+            reverse = (reverse * 10) + lastDgit;
+            console.log("re"+reverse)
+            num1 = Math.floor(num1 / 10);
+            console.log("n"+num1)
+     }
+   
+}
+
+let num1 = prompt("Enter the 1st Number : ");
+
+let reverse=0;
+let lastDgit=0;
+
+palindrome(num1)
+if( num1 == reverse) {
+       console.log("Numbers are Palindrome");
+}
+else {
+       console.log("Numbers are not Palindrome");
+}
