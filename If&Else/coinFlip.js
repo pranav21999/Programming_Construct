3c338d292523e54d956f4093266464bcd24efbd0
pranav21999 <pranavdani99@gmail.com>
2021-08-05 23:06:02 +0530

    Added:programs Of If&ElseStatements

---
+++ If&Else/coinFlip.js
@@ -0,0 +1,11 @@
+/*
+pupuse:generate Random No and print head and tails accordingly
+*/
+const randomNumber = Math.floor( Math.random() * 2 ) + 1;
+
+if ( randomNumber == 1 ) {
+    console.log("Its a HEAD!");
+}
+else {
+    console.log("Its a TAIL!");
+}
